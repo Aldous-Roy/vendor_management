9325b7614b5cb5cc10ce3c739968a0071c424104
Aldous Roy <aldousroy007@gmail.com>
2025-03-09 22:50:22 +0530

    color updates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#C14600',
+        'secondary': '#FF9D23',
+        'accent': '#E5D0AC',
+      },
+    },
   },
   plugins: [],
 }
